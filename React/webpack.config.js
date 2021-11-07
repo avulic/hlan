53d4b70f21c9c1b75d7efe18570b428d38aa0a26
avulic <antevulic96@gmail.com>
2021-11-07 01:59:38 +0100

    Added React, webpack, docker

---
+++ React/webpack.config.js
@@ -0,0 +1,47 @@
+﻿/// <binding />
+"use strict";
+var path = require("path");
+var WebpackNotifierPlugin = require("webpack-notifier");
+var BrowserSyncPlugin = require("browser-sync-webpack-plugin");
+var HtmlWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = {
+    entry: ['babel-polyfill', "./src/index.js"],
+    output: {
+        path: path.resolve(__dirname, "./dist"),
+        filename: "bundle.js",
+        publicPath: '/'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader"
+                }
+            }
+        ]
+    },
+    devtool: "inline-source-map",
+    devServer: {
+        historyApiFallback: true,
+    },
+    plugins: [
+        new WebpackNotifierPlugin(), 
+        new BrowserSyncPlugin(),
+        new HtmlWebpackPlugin({
+            template: './src/index.html'
+        })
+    ],
+    resolve: {
+        extensions: ['.js', '.jsx'],
+        alias: {
+            '@': path.resolve(__dirname, './src'),
+            '@assets': path.resolve(__dirname, './src/components'),
+            '@components': path.resolve(__dirname, './src/components'),
+            '@views': path.resolve(__dirname, './src/views'),
+            '@utils': path.resolve(__dirname, './src/utils')
+        },
+    }
+};
